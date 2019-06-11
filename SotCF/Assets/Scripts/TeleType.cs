552c62cfec78ea10e6080f0febd8268d14cbe5e6
bartoszwieczorek <wiecziiii@gmail.com>
2019-06-11 22:44:04 +0200

    tytuł + niefunkcjonalny skrypt (TeleType)

---
+++ SotCF/Assets/Scripts/TeleType.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TeleType : MonoBehaviour
+{
+
+    private TextMeshProUGUI m_textMeshPro;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        m_textMeshPro = gameObject.GetComponent<TextMeshProUGUI>() ?? gameObject.AddComponent<TextMeshProUGUI>();
+
+        int totalVisibleCharacters = m_textMeshPro.textInfo.characterCount;
+        int counter = 0;
+
+        while (true)
+        {
+            int visibleCount = counter % (totalVisibleCharacters + 1);
+            m_textMeshPro.maxVisibleCharacters = visibleCount;
+
+            if (visibleCount >= totalVisibleCharacters)
+                yield return new WaitForSeconds(1.0f);
+            counter += 1;
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+ 
+}
